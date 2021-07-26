3409f370611625672141f02b7cde1726ade92cdd
KungPaoChick <matthewriel63@gmail.com>
2021-07-26 16:29:39 +0800

    updated

--- dots/configs/polybar/forest/scripts/powermenu.sh
+++ dots/configs/polybar/forest/scripts/powermenu.sh
@@ -57,9 +57,7 @@ case $chosen in
         fi
         ;;
     $lock)
-		if [[ -f /usr/bin/i3lock ]]; then
-			i3lock
-		elif [[ -f /usr/bin/betterlockscreen ]]; then
+		if [[ -f /usr/bin/betterlockscreen ]]; then
 			betterlockscreen -l
 		fi
         ;;
