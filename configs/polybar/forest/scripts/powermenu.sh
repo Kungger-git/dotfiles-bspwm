740d41349c503d91944fefb028a47865c020bcb2
KungPaoChick <matthewriel63@gmail.com>
2021-08-24 20:53:54 +0800

    updated rofi

--- configs/polybar/forest/scripts/powermenu.sh
+++ configs/polybar/forest/scripts/powermenu.sh
@@ -28,7 +28,7 @@ confirm_exit() {
 
 # Message
 msg() {
-	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+	rofi -theme "$dir/message.rasi" -e "Options  -  yes / y / no / n"
 }
 
 # Variable passed to rofi
