844e21b92fe74b56e54c43dc92dcfdb72bc7f9a4
KungPaoChick <matthewriel63@gmail.com>
2021-08-26 09:26:53 +0800

    added fish configuration

---
+++ shells/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    
+    alias ls='ls --color=always -rthla'
+    alias matrix='unimatrix -f -l ocCgGkS -s 96'
+    alias clock='tty-clock -sct -C 4'
+    alias pipes='pipes -p 5 -R -t 1 -r 0'
+    colorscript -r
+end
